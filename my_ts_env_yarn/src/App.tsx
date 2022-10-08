f30e6dc095fcc6919c36149a206eae642ea9166a
konjikun <s1f102200378@iniad.org>
2022-10-09 02:52:00 +0900

    styled-componentsいれた

--- my_ts_env_yarn/src/App.tsx
+++ my_ts_env_yarn/src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
+import styled from "styled-components";
 import "./App.css";
+
+const CustomButton = styled.button`
+  color: red;
+  font-weight: bold;
+`;
 
 function App() {
   return (
@@ -10,6 +16,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <CustomButton>
+          寿限無寿限無後光の擦り切れ回砂利水魚の水行末風来松雲来松くうねる所に住むところ藪ら工事のブラ工事ぱいぽパイポパイ
+        </CustomButton>
         <a
           className="App-link"
           href="https://reactjs.org"
